c0a0796007c050392ddd00e08c0b3b218eb20d12
alextsakpinis <alexandros.tsakpinis@googlemail.com>
2017-10-02 14:36:12 +0200

    Update Atime.py

--- Atime.py
+++ Atime.py
@@ -9,23 +9,26 @@ from query import query
 from lxml import etree
 import re
 
-found = False;
-
 #precompiled xpath queries
 title = etree.XPath('x:title', namespaces = {'x' : 'http://www.mediawiki.org/xml/export-0.10/'})
 textTag = etree.XPath('x:revision/x:text', namespaces = {'x' : 'http://www.mediawiki.org/xml/export-0.10/'})
 
+#Run trough the whole Wikipeadia file and look at every page tag
 for event, element in etree.iterparse('enwiki-20170820-pages-articles-multistream.xml', events=('end',), tag='{http://www.mediawiki.org/xml/export-0.10/}page'):
     
+    #Only Pages which are starting with "A" or "a" are considered.
     if re.match('[Aa]',title(element)[0].text):
         text = textTag(element)[0].text            
         
+        #Queries are only executed if there is a text
         if text != None:
-    
+            
+            #Run the queries on the previous saved text
             query('"cats" [0,10] "are" [0,10] "to"', text)
             query('"or" [0,10] "or" [0,10] "or"', text)
             query('"when" [15,25] "republic" [15,25] "along"', text)          
-         
+    
+    #Clear the element from the memory in order to be able to go through the whole Wikipedia file
     element.clear()
     while element.getprevious() is not None:
         del element.getparent()[0]
