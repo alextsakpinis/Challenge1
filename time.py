45904155d484bf60149ed14b6b0722adc8ae02ac
alextsakpinis <alexandros.tsakpinis@googlemail.com>
2017-10-02 14:00:45 +0200

    Update time.py

--- time.py
+++ time.py
@@ -14,14 +14,18 @@ found = False;
 title = etree.XPath('x:title', namespaces = {'x' : 'http://www.mediawiki.org/xml/export-0.10/'})
 textTag = etree.XPath('x:revision/x:text', namespaces = {'x' : 'http://www.mediawiki.org/xml/export-0.10/'})
 
+#Run trough the whole Wikipeadia file and look at every page tag
 for event, element in etree.iterparse('enwiki-20170820-pages-articles-multistream.xml', events=('end',), tag='{http://www.mediawiki.org/xml/export-0.10/}page'):
     
+    #Takes the text section of every page tag
     text = textTag(element)[0].text            
     
+    #Run the queries on the previous saved text
     query('"cats" [0,10] "are" [0,10] "to"', text)
     query('"or" [0,10] "or" [0,10] "or"', text)
     query('"when" [15,25] "republic" [15,25] "along"', text) 
-        
+    
+    #Clear the element from the memory in order to be able to go through the whole Wikipedia file
     element.clear()
     while element.getprevious() is not None:
         del element.getparent()[0]
